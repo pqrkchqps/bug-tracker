32178f6e3d70417c9c10ace2131e7a9ecec3bfca
john <johnacreps@gmail.com>
2020-09-15 17:20:47 -0500

    CreateBugForm is console logging the submitted data

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import './styles/styles.scss'
 import CreateBugForm from './components/CreateBugForm'
--- react-frontend/src/components/CreateBugForm.js
+++ react-frontend/src/components/CreateBugForm.js
@@ -1,11 +1,60 @@
 import React from 'react'
-//import Component from 'react'
 
 class CreateBugForm extends React.Component {
   constructor(props){
     super(props);
 
-    //this.state
+    this.state = {
+      bugName: '',
+      summary: '',
+      severity: '',
+      status: '',
+      createdBy: '',
+      assignedTo: '',
+      version: '',
+      timeEstimate: '',
+      deadline: '',
+      hoursWorked: '',
+      percentComplete: ''
+    }
+  }
+  bugNameChangeHandler = (e) => {
+    this.setState({bugName: e.target.value});
+  }
+  summaryChangeHandler = (e) => {
+    this.setState({summary: e.target.value});
+  }
+  severityChangeHandler = (e) => {
+    this.setState({severity: e.target.value});
+  }
+  createdByChangeHandler = (e) => {
+    this.setState({createdBy: e.target.value});
+  }
+  statusChangeHandler = (e) => {
+    this.setState({status: e.target.value});
+  }
+  assignedToChangeHandler = (e) => {
+    this.setState({assignedTo: e.target.value});
+  }
+  versionChangeHandler = (e) => {
+    this.setState({version: e.target.value});
+  }
+  timeEstimateChangeHandler = (e) => {
+    this.setState({timeEstimate: e.target.value});
+  }
+  deadlineChangeHandler = (e) => {
+    this.setState({deadline: e.target.value});
+  }
+  hoursWorkedChangeHandler = (e) => {
+    this.setState({hoursWorked: e.target.value});
+  }
+  percentCompleteChangeHandler = (e) => {
+    this.setState({percentComplete: e.target.value});
+  }
+
+  submitHandler = (e) => {
+    e.preventDefault();
+    console.log(this.state);
   }
   /*
   Status and resolution - what state is the bug in (not even confirmed a bug to fix confirm)
@@ -20,51 +69,55 @@ class CreateBugForm extends React.Component {
   */
   render(){
     return (
-      <form id="create-bug-form">
-        <h3 class="title">Create A New Bug</h3>
-        <div class="form-box">
-          <label class="form-label">Bug Name</label>
-          <input class="form-input" type="text" />
+      <form id="create-bug-form" onSubmit={this.submitHandler}>
+        <h3 className="title">Create A New Bug</h3>
+        <div className="form-box">
+          <label className="form-label">Bug Name</label>
+          <input className="form-input" onChange={this.bugNameChangeHandler} type="text" />
         </div>
-        <div class="form-box">
-          <label class="form-label">Summary</label>
-          <input class="form-input" type="text" />
+        <div className="form-box">
+          <label className="form-label">Summary</label>
+          <input className="form-input" onChange={this.summaryChangeHandler} type="text" />
         </div>
-        <div class="form-box">
-          <label class="form-label">Severity</label>
-          <input class="form-input" type="text" />
+        <div className="form-box">
+          <label className="form-label">Severity</label>
+          <input className="form-input" onChange={this.severityChangeHandler} type="text" />
         </div>
-        <div class="form-box">
-          <label class="form-label">Status</label>
-          <input class="form-input" type="text" />
+        <div className="form-box">
+          <label className="form-label">Status</label>
+          <input className="form-input" onChange={this.statusChangeHandler} type="text" />
         </div>
-        <div class="form-box">
-          <label class="form-label">Created By</label>
-          <input class="form-input" type="text" />
+        <div className="form-box">
+          <label className="form-label">Created By</label>
+          <input className="form-input" onChange={this.createdByChangeHandler} type="text" />
         </div>
-        <div class="form-box">
-          <label class="form-label">Assigned To</label>
-          <input class="form-input" type="text" />
+        <div className="form-box">
+          <label className="form-label">Assigned To</label>
+          <input className="form-input" onChange={this.assignedToChangeHandler} type="text" />
         </div>
-        <div class="form-box">
-          <label class="form-label">Version Found In</label>
-          <input class="form-input" type="text" />
+        <div className="form-box">
+          <label className="form-label">Version Found In</label>
+          <input className="form-input" onChange={this.versionChangeHandler} type="text" />
         </div>
-        <div class="form-box">
-          <label class="form-label">Time Estimate</label>
-          <input class="form-input" type="text" />
+        <div className="form-box">
+          <label className="form-label">Time Estimate</label>
+          <input className="form-input" onChange={this.timeEstimateChangeHandler} type="text" />
         </div>
-        <div class="form-box">
-          <label class="form-label">Deadline</label>
-          <input class="form-input" type="text" />
+        <div className="form-box">
+          <label className="form-label">Deadline</label>
+          <input className="form-input" onChange={this.deadlineChangeHandler} type="text" />
         </div>
-        <div class="form-box">
-          <label class="form-label">Hours Worked</label>
-          <input class="form-input" type="text" />
+        <div className="form-box">
+          <label className="form-label">Hours Worked</label>
+          <input className="form-input" onChange={this.hoursWorkedChangeHandler} type="text" />
         </div>
-        <div class="form-box">
-          <label class="form-label">Percent Complete</label>
-          <input class="form-input" type="text" />
+        <div className="form-box">
+          <label className="form-label">Percent Complete</label>
+          <input className="form-input" onChange={this.percentCompleteChangeHandler} type="text" />
+        </div>
+        <div className="form-box">
+          <label className="form-label">Submit</label>
+          <input className="form-input" type="submit" />
         </div>
       </form>
     )
