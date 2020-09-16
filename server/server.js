8c629d57cec342f55e3d08e327b476e8d72dc330
john <johnacreps@gmail.com>
2020-09-16 18:59:28 -0500

    Moved server out of react-frontend, created package.json for the bug-tracker, server loads up react-frontend correctly

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'build');
+const publicPath = path.join(__dirname, '../react-frontend', 'build');
 
 const port = process.env.PORT || 5000;
 
